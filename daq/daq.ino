e7309dd8d09f1167ac2d164922272baa6cf4ca9e
J. Sullivan Muse <sullivan.muse@gmail.com>
2024-10-24 18:18:13 -0500

    Implement DAQ for 2 HX711s

--- daq/daq.ino
+++ daq/daq.ino
@@ -1,9 +1,36 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "HX711.h"
 
+#define DOUT 13
+#define CLK 12
+
+#define DOUT1 11
+#define CLK1 10
+
+HX711 scale;
+HX711 scale1;
+
+float calibration_factor = 84600;
+
+void setup() {
+  Serial.begin(9600);
+
+  scale.begin(DOUT, CLK);
+  scale.set_scale();
+  scale.tare();
+
+  scale1.begin(DOUT1, CLK1);
+  scale1.set_scale();
+  scale1.tare();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  scale.set_scale(calibration_factor);
+  scale1.set_scale(calibration_factor);
 
+  Serial.print("Reading scale 0: ");
+  Serial.print(scale.get_units(), 1);
+  Serial.print(" lbs, Reading scale 1: ");
+  Serial.print(scale1.get_units(), 1);
+  Serial.print(" lbs");
+  Serial.println();
 }
